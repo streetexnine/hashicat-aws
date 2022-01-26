5610b0247e56e64d533b1eaa61049041aeb5a8e1
streetexnine <dmatius8@gmail.com>
2022-01-26 15:01:44 +0000

    anjir cape

--- main.tf
+++ main.tf
@@ -119,8 +119,6 @@ resource "aws_eip_association" "hashicat" {
 }
 
 resource "aws_instance" "hashicat" {
-  department                   = devops
-  billable                    = true
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.instance_type
   key_name                    = aws_key_pair.hashicat.key_name
@@ -130,6 +128,8 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
+    Department                  = devops
+    Billable                    = true
   }
 }
 
