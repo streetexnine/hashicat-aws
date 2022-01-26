5cfba64132e626774e5da07103f940051279d1b3
streetexnine <dmatius8@gmail.com>
2022-01-26 10:45:26 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,9 @@ module "s3-bucket" {
   source  = "app.terraform.io/ivatama-david/s3-bucket/aws"
   version = "2.8.0"
   # insert required variables here
-  bucket_prefix = "dmatius8"
+  variable "bucket_prefix" {
+      type        = string
+      description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
+      default     = "dmatius8"
+  }
 }
