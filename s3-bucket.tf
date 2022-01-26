d7f976522fbf18190a0fe7e4e0d0346771d877c1
streetexnine <dmatius8@gmail.com>
2022-01-26 13:15:46 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,5 @@ module "s3-bucket" {
   source  = "app.terraform.io/ivatama-david/s3-bucket/aws"
   version = "2.8.0"
   # insert required variables here
-  bucket_prefix = "dmatius8" 
+  bucket_prefix = "dmatius8"
 }
